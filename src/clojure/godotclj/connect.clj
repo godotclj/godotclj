87f428a53c11dd0a35e95a8193c3643bf0efbedf
3starblaze <3starblaze@protonmail.com>
2022-01-14 12:24:02 +0200

    Cleanup connect ns
    - Use `util/get-helper-node`
    - Remove redundant "WARNING: " from util/warn! call

--- src/clojure/godotclj/connect.clj
+++ src/clojure/godotclj/connect.clj
@@ -8,15 +8,14 @@
   Implementation details:
   When `connect` is called, `signal-registry-handler` checks whether the signal
   is not already connected and if it is not, then `::signal-entry` is made and
-  is stored in `signal-registry`. Then Godot signal is connected to signal
-  node's (`get-signal-node`) method `signal-handler-method-name` and binds
-  `::signal-entry` id with it.
+  is stored in `signal-registry`. Then Godot signal is connected to the helper
+  node's  method `signal-handler-method-name` and binds `::signal-entry` id
+  with it.
 
   When a signal is fired, `signal-handler-method-name` method receives signal
   arguments and the `::signal-entry` id. The id is used to find the signal
   entry, grab `:f` and apply signal arguments to it."
   (:require
-   [godotclj.api :as api]
    [godotclj.bindings.godot :as godot]
    [godotclj.registry-utils :as registry-utils]
    [godotclj.util :as util]))
@@ -31,17 +30,6 @@
 
 (def ^:private signal-registry
   (registry-utils/make-registry {} signal-registry-spec))
-
-;; HACK That node might disappear
-(defn- get-signal-node
-  []
-  (->> (api/->object "_Engine")
-       .getMainLoop
-       .getRoot
-       .getChildren
-       godot/array->seq
-       first
-       (api/->object "Object")))
 
 (defn- signal-registered?
   "Check `registry` if there is a handler for `signal-name` on `node`."
@@ -60,7 +48,7 @@
       (do
         (.connect node
                   signal-name
-                  (get-signal-node)
+                  (util/get-helper-node)
                   signal-handler-method-name
                   [instance-id signal-name])
         (assoc-in registry [(.getInstanceId node) signal-name] f)))))
@@ -72,12 +60,12 @@
       (do
         (.disconnect node
                      signal-name
-                     (get-signal-node)
+                     (util/get-helper-node)
                      signal-handler-method-name)
         (dissoc registry instance-id signal-name))
       (do (util/warn!
            (format
-            "WARNING: No function to disconnect on object %s with signal %s!\n"
+            "No function to disconnect on object %s with signal %s!\n"
             node
             signal-name))
           registry))))
